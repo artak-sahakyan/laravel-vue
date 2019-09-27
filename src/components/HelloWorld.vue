7714cc198772956b72f370ada34b7f2808154093
artak-sahakyan <web.artaksahakyan@gmail.com>
2019-09-27 18:51:30 +0400

    init

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,126 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+  <v-simple-table>
+    <template v-slot:default>
+      <thead>
+
+        <tr>
+          <th v-for="(field, fieldName) in fields">
+            <v-text-field v-model="filterFields[fieldName]" @input="getProperties"></v-text-field>
+          </th>
+        </tr>
+
+        <tr>
+          <th v-for="(field, fieldName) in fields" @click="setSort(fieldName)">{{field}}</th>
+        </tr>
+
+      </thead>
+
+      <tbody>
+        <tr v-for="property in properties" :key="property.title">
+          <td v-for="(field, fieldName) in fields">{{ property[fieldName] }}</td>
+        </tr>
+      </tbody>
+    </template>
+  </v-simple-table>
 </template>
 
 <script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
-</script>
+    export default {
+        data () {
+            return {
+                properties: [],
+                page: 1,
+                sortField: null,
+                sortType: null,
+                filterFields: {
+                },
+                fields: {
+                    title: 'Title',
+                    description: 'Description',
+                    bedroom: 'Bedroom',
+                    bathroom: 'bathroom',
+                    propertyType: 'Property Type',
+                    status: 'Status',
+                    forSale: 'for sale',
+                    forRent: 'forRent',
+                    projectName: 'projectName',
+                    country: 'country',
+                }
+            }
+        },
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
+        methods: {
+            setSort(field) {
+              if(this.sortField == field) {
+                  this.sortType = this.sortType == 'ASC' ? 'DESC' : 'ASC';
+              } else {
+                  this.sortField = field;
+                  this.sortType = 'ASC';
+              }
+              this.getProperties();
+            },
+
+            getProperties() {
+
+
+                let requestData = {
+                    filterFields: this.filterFields,
+                    sortField: this.sortField,
+                    sortType: this.sortType,
+                    page: this.page
+                }
+
+
+                console.log(requestData);
+                setTimeout( () => {
+                    this.properties = [
+                        {
+                            title: 'sadasdss',
+                            description: 'descasda',
+                            bedroom: 3,
+                            bathroom: 5,
+                            propertyType: 'Condo',
+                            status: 'sadasd',
+                            forSale: 'sSsssd',
+                            forRent: 'sSsssd',
+                            projectName: 'ssdfsd ds dsf Ssssd',
+                            country: 'Armenia',
+
+                        },
+                        {
+                            title: 'sadasd',
+                            description: 'descasda',
+                            bedroom: 3,
+                            bathroom: 5,
+                            propertyType: 'Condo',
+                            status: 'sadasd',
+                            forSale: 'sSsssd',
+                            forRent: 'sSsssd',
+                            projectName: 'ssdfsd ds dsf Ssssd',
+                            country: 'Armenia',
+
+                        },
+                        {
+                            title: 'sadggasd',
+                            description: 'descasda',
+                            bedroom: 3,
+                            bathroom: 5,
+                            propertyType: 'Condo',
+                            status: 'sadasd',
+                            forSale: 'sSsssd',
+                            forRent: 'sSsssd',
+                            projectName: 'ssdfsd ds dsf Ssssd',
+                            country: 'Armenia',
+
+                        },
+                    ]
+                })
+            }
+        },
+
+        created() {
+          this.getProperties();
+        }
+
+    }
+</script>
