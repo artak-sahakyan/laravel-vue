b005f32144f060c175b4982582ed80ec9b049c16
artak-sahakyan <web.artaksahakyan@gmail.com>
2019-09-29 18:29:43 +0400

    vue

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,39 +1,40 @@
 <template>
-  <v-simple-table>
-    <template v-slot:default>
-      <thead>
+    <v-simple-table>
+        <template v-slot:default>
+            <thead>
 
-        <tr>
-          <th v-for="(field, fieldName) in fields">
-            <v-text-field v-model="filterFields[fieldName]" @input="getProperties"></v-text-field>
-          </th>
-        </tr>
+            <tr>
+                <th v-for="(field, fieldName) in fields">
+                    <v-text-field v-model="filterFields[fieldName]" @input="getProperties"></v-text-field>
+                </th>
+            </tr>
 
-        <tr>
-          <th v-for="(field, fieldName) in fields" @click="setSort(fieldName)">{{field}}</th>
-        </tr>
+            <tr>
+                <th v-for="(field, fieldName) in fields" @click="setSort(fieldName)">{{field}}</th>
+            </tr>
 
-      </thead>
+            </thead>
 
-      <tbody>
-        <tr v-for="property in properties" :key="property.title">
-          <td v-for="(field, fieldName) in fields">{{ property[fieldName] }}</td>
-        </tr>
-      </tbody>
-    </template>
-  </v-simple-table>
+            <tbody>
+            <tr v-for="property in properties" :key="property.title">
+                <td v-for="(field, fieldName) in fields">{{ property[fieldName] }}</td>
+            </tr>
+            </tbody>
+        </template>
+    </v-simple-table>
 </template>
 
 <script>
+    import axios from 'axios'
     export default {
         data () {
             return {
+                url:'http://laravel-api.loc',
                 properties: [],
                 page: 1,
                 sortField: null,
                 sortType: null,
-                filterFields: {
-                },
+                filterFields: {},
                 fields: {
                     title: 'Title',
                     description: 'Description',
@@ -51,17 +52,16 @@
 
         methods: {
             setSort(field) {
-              if(this.sortField == field) {
-                  this.sortType = this.sortType == 'ASC' ? 'DESC' : 'ASC';
-              } else {
-                  this.sortField = field;
-                  this.sortType = 'ASC';
-              }
-              this.getProperties();
+                if (this.sortField == field) {
+                    this.sortType = this.sortType == 'ASC' ? 'DESC' : 'ASC';
+                } else {
+                    this.sortField = field;
+                    this.sortType = 'ASC';
+                }
+                this.getProperties();
             },
 
             getProperties() {
-
 
                 let requestData = {
                     filterFields: this.filterFields,
@@ -71,8 +71,22 @@
                 }
 
 
+                axios
+                    .post(this.url+'/api/properties', requestData, {
+                        headers: {
+                            'Content-Type': 'application/json'
+                        }
+                    })
+                    .then(response => {
+                        console.log(response);
+                    })
+                    .catch(error => {
+                        console.log(error)
+                    })
+
+
                 console.log(requestData);
-                setTimeout( () => {
+                setTimeout(() => {
                     this.properties = [
                         {
                             title: 'sadasdss',
@@ -115,11 +129,28 @@
                         },
                     ]
                 })
+            },
+            getFilterOptions() {
+                axios
+                    .get(this.url + '/api/filter-options', {
+                        headers: {
+                            'Content-Type': 'application/json'
+                        }
+                    })
+                    .then(response => {
+
+                    })
+                    .catch(error => {
+                        console.log(error)
+                    })
+
             }
         },
 
         created() {
-          this.getProperties();
+
+            this.getFilterOptions();
+            this.getProperties();
         }
 
     }
